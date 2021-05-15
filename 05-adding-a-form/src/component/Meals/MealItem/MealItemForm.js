e329b657ee7a63636b2ec8f69bd9fc815160de9f
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-16 01:16:09 +0530

    A form added to add into Cart and Reusable Input added

---
+++ 05-adding-a-form/src/component/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,29 @@
+import Input from "../../UI/Input/Input";
+
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
