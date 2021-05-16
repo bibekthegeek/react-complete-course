5356d2a44d55d3d98c65059d3d4b8a3693a9ea39
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-17 00:46:14 +0530

    Removing item from cart added

---
+++ 11-making-items-removable/src/component/Cart/Cart.js
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import Modal from "../UI/Modal/Modal";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItemReoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          // summary={}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemReoveHandler.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onCloseCart={props.onCloseCart}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
