9b6356608888086370d824efc6871f1b87c8d696
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-16 17:16:45 +0530

    Output cart items

---
+++ 10-outputting-cart-items/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Cart from "./component/Cart/Cart";
+import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setShowCart(false);
+  };
+
+  return (
+    <CartProvider>
+      {showCart && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
