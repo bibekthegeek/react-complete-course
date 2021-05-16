cd24e9e03f2d4f359d36885a15dfa279157f6c8d
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-16 12:39:26 +0530

    Adding a Modal via a React Portal

---
+++ 07-adding-a-modal/src/App.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import Cart from "./component/Cart/Cart";
+import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
