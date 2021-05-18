a609ffd1f1e0355e737a207e514a3d430c99e7b0
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-18 10:55:18 +0530

    Added sending get request

---
+++ 02-sending-a-get-request/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  // const fetchMovieHandler = () => {
+  //   fetch("https://swapi.dev/api/films/").then((result) => {
+  //     result.json().then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           release: movieData.release_date,
+  //         };
+  //       });
+
+  //       setMovies(transformedMovies);
+  //     });
+  //   });
+  // };
+
+  async function fetchMovieHandler() {
+    const result = await fetch("https://swapi.dev/api/films/");
+    const data = await result.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        release: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
