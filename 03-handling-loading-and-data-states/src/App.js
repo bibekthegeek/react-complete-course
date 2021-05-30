9427da53cc4f8bb663283880c020440b77b796c5
Bibekthegeek <bibekthegeek@gmail.com>
2021-05-30 13:35:53 +0530

    Handling loading & Data States

---
+++ 03-handling-loading-and-data-states/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // const fetchMovieHandler = () => {
+  //   fetch("https://swapi.dev/api/films/").then((result) => {
+  //     result.json().then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           release: movieData.release_date,
+  //         };
+  //       });
+
+  //       setMovies(transformedMovies);
+  //     });
+  //   });
+  // };
+
+  async function fetchMovieHandler() {
+    setIsLoading(true);
+    const result = await fetch("https://swapi.dev/api/films/");
+    const data = await result.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        release: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
